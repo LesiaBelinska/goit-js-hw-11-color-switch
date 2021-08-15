01e51d53a2a0b3e2a56c7bb6aa349e0686985575
LesiaBelinska <lm.kudak@gmail.com>
2021-08-16 00:10:32 +0300

    index.js_done

--- index.js
+++ index.js
@@ -11,3 +11,48 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+class colorPicker  {
+    constructor() { 
+        this.intervalId = null;
+        this.isActive = false;
+    }
+    
+    onStartBtnClick(evt) {
+        if (this.isActive) {
+            return;
+        }
+      
+      this.isActive = true;
+      this.intervalId = setInterval(() => {
+          
+        getBodyColor(colors)
+        startColorPickerBtn.setAttribute("disabled", "disabled");
+        
+        }, 1000);
+      
+}
+
+    onStopBtnClick(evt) {
+        clearInterval(this.intervalId);
+      this.isActive = false;
+      startColorPickerBtn.removeAttribute("disabled");
+}
+}
+
+
+function setBodyColor(colorHex) {
+  document.body.style.backgroundColor = colorHex;
+}
+
+function getBodyColor(colors) {
+    let colorIndex = randomIntegerFromInterval(0, colors.length);
+    let colorHex = colors[colorIndex];
+    setBodyColor(colorHex);
+}
+const copyOfColorPicker = new colorPicker(); 
+
+const startColorPickerBtn = document.querySelector('button[data-action= "start"]');
+const stopColorPickerBtn = document.querySelector('button[data-action= "stop"]');
+startColorPickerBtn.addEventListener('click', copyOfColorPicker.onStartBtnClick.bind(copyOfColorPicker));
+stopColorPickerBtn.addEventListener('click', copyOfColorPicker.onStopBtnClick.bind(copyOfColorPicker));
+
